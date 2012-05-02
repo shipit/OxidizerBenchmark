f6aa348c292b4f659700aa7d9f6472e03de231db
Sumeet Parmar <z.sumeet@gmail.com>
2012-05-02 15:27:31 -0700

    Initial Commit

---
+++ OxidizerBenchmark/OBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OBAppDelegate.h
+//  OxidizerBenchmark
+//
+//  Created by Sumeet Parmar on 5/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
